9824eb5d96732874f7977f54fb8f666e7708f293
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-11-24 21:17:46 +0530

    Update marvell.py

--- marvell.py
+++ marvell.py
@@ -254,7 +254,6 @@ if authenticate_user():
             except:                   
                   output = fs_chain(f'You need to fix the code but ONLY produce SQL code output. If the question is complex, consider using one or more CTE. Examine the DDL statements and answer this question: {output}')
                   st.write(sf_query(output['result']))
-        except Exception as error:  
-          st.write(error)		
+        except Exception as error:           		
           with st.chat_message("assistant"):
             st.markdown("Data for the provided question is not available. Please try to improve your question. ")
