02da75b0a63e9914e71bc076cadb8cc2e5ad4df4
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-11-28 16:55:46 +0530

    Update marvell.py

--- marvell.py
+++ marvell.py
@@ -235,7 +235,7 @@ if authenticate_user():
         try:
             #st.write(str_input)
             output = fs_chain(str_input)
-            #st.write(output)
+            st.write(output)
             #st.write(output['result'])
             try:
                 # if the output doesn't work we will try one additional attempt to fix it
