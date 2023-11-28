ebd7f37550ab8b21f87c8b48539b322df722aaf0
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-11-28 18:08:16 +0530

    Update marvell.py

--- marvell.py
+++ marvell.py
@@ -217,6 +217,7 @@ if authenticate_user():
         with st.chat_message(message["role"]):
             role = message["role"]
             df_str = message["content"]
+            st.write(df_str)
             if role == "user":
                 st.markdown(message["content"], unsafe_allow_html = True)
                 continue
@@ -254,8 +255,8 @@ if authenticate_user():
                     headers = df_2.columns
                     st.markdown(tabulate(df_2, tablefmt="html",headers=headers,showindex=False), unsafe_allow_html = True) 
                     st.write(analysis)
-                  st.session_state.messages.append({"role": "assistant", "content": df_2.to_csv(sep=',', index=False)})
-                  st.session_state.messages.append({"role": "assistant", "content": analysis})
+                  st.session_state.messages.append({"role": "assistant", "content": df_2.to_csv(sep=',', index=False) + analysis})
+                  #st.session_state.messages.append({"role": "assistant", "content": analysis})
                   
                 else:
                   with st.chat_message("assistant"):
