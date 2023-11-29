3c341bb3c14b912c0a4bbb55592f07817d6ba005
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-11-29 12:27:30 +0530

    Update marvell.py

--- marvell.py
+++ marvell.py
@@ -196,6 +196,8 @@ if authenticate_user():
     with st.sidebar:
       image = Image.open("assets/jadeglobal.png")
       image = st.image('assets/jadeglobal.png',width=280)
+      st.markdown(""" SCM Inventory Management GenAI Assistant """)
+      
       
     str_input = st.chat_input("Enter your question:")
     st.markdown("""
